c87af1d96abf64c530cd77050d21d5dc4c0dfa81
Soumya Ray <soumya.ray@gmail.com>
2018-03-01 20:52:47 +0800

    Refactor entropy method

--- entropy.rb
+++ entropy.rb
@@ -1,11 +1,22 @@
-def entropy(string)
-  length = string.length
-  counts = string.chars.uniq
-                 .map { |c| [c, string.count(c).to_f] }
-                 .to_h
+# def entropy(string)
+#   counts = string.chars.uniq
+#                  .map { |c| string.count(c).to_f }
 
-  counts.values.reduce(0) do |entropy, count|
-    prob = count / length
-    entropy - prob * Math.log2(prob)
-  end
+#   counts.reduce(0) do |entropy, count|
+#     prob = count / string.length
+#     entropy - prob * Math.log2(prob)
+#   end
+# end
+
+def entropy(string)
+  string
+    .chars.uniq
+    .map { |c| string.count(c).to_f }
+    .reduce(0) do |entropy, count|
+      prob = count / string.length
+      entropy - prob * Math.log2(prob)
+    end
 end
+
+# entropy('aaaabcdd') == 1.75
+# entropy('abcd')    == 2.00
