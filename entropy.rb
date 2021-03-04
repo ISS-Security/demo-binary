6adf2a1b1fcd97ff062bfbc79ae9e3426c1df884
Soumya Ray <soumya.ray@gmail.com>
2021-03-04 20:20:24 +0800

    add unidiomatic entropy method

--- entropy.rb
+++ entropy.rb
@@ -1,13 +1,18 @@
-# def entropy(string)
-#   counts = string.chars.uniq
-#                  .map { |c| string.count(c).to_f }
+# unidiomatic ruby entropy method
+def entropy(string)
+  e = 0
+  sz = string.bytesize.to_f
+  b = string.bytes
+  0.upto(255) do |i|
+    x = b.count(i)/sz
+    if x > 0
+      e += -x * (Math.log(x) / Math.log(2))
+    end
+  end
+  e
+end
 
-#   counts.reduce(0) do |entropy, count|
-#     prob = count / string.length
-#     entropy - prob * Math.log2(prob)
-#   end
-# end
-
+# idiomatic ruby entropy method
 def entropy(string)
   string
     .chars.uniq
