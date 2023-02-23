fba9e7699c33831a3af083f32641eeb9b5324412
Soumya Ray <soumya.ray@gmail.com>
2023-02-23 20:52:12 +0800

    Add manual entropy calculations

--- entropy.rb
+++ entropy.rb
@@ -1,3 +1,12 @@
+# [A, A, A, A, B, C, D, D]
+(0.5) * Math.log2(0.5) +
+(0.125) * Math.log2(0.125) +
+(0.125) * Math.log2(0.125) +
+(0.25) * Math.log2(0.25)
+
+# [A, B, C, D]
+-4 * (0.25) * Math.log2(0.25)
+
 # unidiomatic ruby entropy method
 def entropy(string)
   e = 0
